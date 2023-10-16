cf7d195e7bc1569f1ccefa3908916ee99f491e91
Munris <munris.tech@gmail.com>
2023-10-16 21:45:37 +0300

    add custom rpc

--- config.ts
+++ config.ts
@@ -1,44 +1,49 @@
 export const generalConfig = {
-    sleepFrom: 60,
-    sleepTo: 150,
-    maxGas: 7,
-    maxAddressTxCount: 10,
+    rpc: 'https://rpc.ankr.com/eth',
+    sleepFrom: 200,
+    sleepTo: 300,
+    maxGas: 15,
+    maxAddressTxCount: 100,
     maxTxPerRun: 3,
     shuffleModules: false,
     modules: [
-        'base_bridge',
         'zora_bridge',
-        'zk_bridge',
-        'starknet_bridge',
-        'transfer',
-        'bungee_refuel',
-        'mintfun'
+        'bungee',
+        'transfer'
     ]
+
+    // 'base_bridge'
+    // 'zora_bridge'
+    // 'zk_bridge'
+    // 'starknet_bridge'
+    // 'bungee'
+    // 'mintfun'
+    // 'transfer'
 }
 
 export const baseBridgeConfig = {
     bridgeFrom: 0.001,
-    bridgeTo: 0.002
+    bridgeTo: 0.0012
 }
 
 export const zoraBridgeConfig = {
     bridgeFrom: 0.001,
-    bridgeTo: 0.002
+    bridgeTo: 0.0012
 }
 
 export const zkBridgeConfig = {
     bridgeFrom: 0.001,
-    bridgeTo: 0.002
+    bridgeTo: 0.0012
 }
 
 export const starknetBridgeConfig = {
-    bridgeFrom: 0.001,
-    bridgeTo: 0.002
+    bridgeFrom: 0.0005,
+    bridgeTo: 0.0006
 }
 
 export const transferConfig = {
-    percentSendFrom: 85,
-    percentSendTo: 90
+    percentSendFrom: 95,
+    percentSendTo: 97
 }
 
 export const bungeeBridgeConfig = {
--- utils/ethClient.ts
+++ utils/ethClient.ts
@@ -9,14 +9,52 @@ import {
     WalletClient
 } from "viem"
 import {privateKeyToAccount} from "viem/accounts"
-import {mainnet} from "viem/chains"
+import { generalConfig } from "../config"
+
+const eth = defineChain({
+    id: 1,
+    network: 'homestead',
+    name: 'Ethereum',
+    nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+    rpcUrls: {
+        default: {
+            http: [generalConfig.rpc],
+        },
+        public: {
+            http: [generalConfig.rpc],
+        },
+    },
+    blockExplorers: {
+        etherscan: {
+            name: 'Etherscan',
+            url: 'https://etherscan.io',
+        },
+        default: {
+            name: 'Etherscan',
+            url: 'https://etherscan.io',
+        },
+    },
+    contracts: {
+        ensRegistry: {
+            address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+        },
+        ensUniversalResolver: {
+            address: '0xc0497E381f536Be9ce14B0dD3817cBcAe57d2F62',
+            blockCreated: 16966585,
+        },
+        multicall3: {
+            address: '0xca11bde05977b3631167028862be2a173976ca11',
+            blockCreated: 14353601,
+        },
+    },
+})
 
 function getPublicEthClient(): PublicClient {
-    return createPublicClient({ chain: mainnet, transport: http() })
+    return createPublicClient({ chain: eth, transport: http() })
 }
 
 function getEthWalletClient(privateKey: Hex): WalletClient<HttpTransport, Chain, PrivateKeyAccount> {
-    return createWalletClient({ chain: mainnet, account: privateKeyToAccount(privateKey), transport: http() })
+    return createWalletClient({ chain: eth, account: privateKeyToAccount(privateKey), transport: http() })
 }
 
 export { getPublicEthClient, getEthWalletClient }
